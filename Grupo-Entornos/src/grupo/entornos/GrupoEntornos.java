dc1429727dd74a1001bbb558e1ebac79423aa4bb
WEB1-26 <WEB1-26@C23-05.JOVELLANOS.EDU>
2026-03-10 20:25:05 +0100

    Primer cambio

---
+++ Grupo-Entornos/src/grupo/entornos/GrupoEntornos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo.entornos;
+
+/**
+ *
+ * @author WEB1-26
+ */
+public class GrupoEntornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // MARIUS PRUEBA GRUPO GIT
+    }
+    
+}
